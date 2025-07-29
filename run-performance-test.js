8a817f07cf90c9bc1b29f8d8363794fa4ade358c
Eduardo Terroso <emterroso@gmail.com>
2025-07-29 13:54:32 +0200

    cleanup

--- run-performance-test.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// Enable garbage collection for memory measurements
-if (process.argv.includes('--expose-gc')) {
-    console.log('Garbage collection enabled for memory measurements');
-}
-
-// Run the performance test
-import('./test/performance/interval-tree-performance.js').catch(console.error); 
